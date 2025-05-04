cf82f367c09da0023189fe3d9c3571c849314a34
Royna2544 <whiteshell2544@naver.com>
2025-05-04 14:16:42 +0900

    script: Add simple script to regen/merge configs

---
+++ scripts/regen_defconfig.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+OUTDIR=out
+
+for def in arch/arm64/configs/*_defconfig; do
+	make O=$OUTDIR $(basename $def)
+	cp $OUTDIR/.config $OUTDIR/$(basename $def);
+	make O=$OUTDIR savedefconfig;
+	cp $OUTDIR/defconfig $def;
+done
