0f9ff9641f390fd7f12a96a2ab1bf607391f7063
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2026-01-14 11:01:56 +0000

    crypto: acompress, scompress: Replaced gcc specific attributes with macros from compiler.h
     * Base commit: d8c34b949d8c9f61e099e00f22770e400adf2b76 (ACK)
     * Done separately because these weren't available in Linux Kernel 4.4
    
    Change-Id: I95fe33374ddbb81d49a30707a0a7f1b010921bea
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- crypto/acompress.c
+++ crypto/acompress.c
@@ -21,6 +21,7 @@
 #include <crypto/algapi.h>
 #include <linux/cryptouser.h>
 #include <net/netlink.h>
+#include <linux/compiler.h>
 #include <crypto/internal/acompress.h>
 #include <crypto/internal/scompress.h>
 #include "internal.h"
@@ -50,7 +51,7 @@ static int crypto_acomp_report(struct sk_buff *skb, struct crypto_alg *alg)
 #endif
 
 static void crypto_acomp_show(struct seq_file *m, struct crypto_alg *alg)
-	__attribute__ ((unused));
+	__maybe_unused;
 
 static void crypto_acomp_show(struct seq_file *m, struct crypto_alg *alg)
 {
--- crypto/scompress.c
+++ crypto/scompress.c
@@ -21,6 +21,7 @@
 #include <linux/vmalloc.h>
 #include <crypto/algapi.h>
 #include <linux/cryptouser.h>
+#include <linux/compiler.h>
 #include <net/netlink.h>
 #include <linux/scatterlist.h>
 #include <crypto/scatterwalk.h>
@@ -57,7 +58,7 @@ static int crypto_scomp_report(struct sk_buff *skb, struct crypto_alg *alg)
 #endif
 
 static void crypto_scomp_show(struct seq_file *m, struct crypto_alg *alg)
-	__attribute__ ((unused));
+	__maybe_unused;
 
 static void crypto_scomp_show(struct seq_file *m, struct crypto_alg *alg)
 {
